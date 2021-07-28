df6b6cfb57d26ba329ddad27d8c86330f8bdf588
Robin Reiter <reiter.code@gmail.com>
2021-07-28 14:46:31 +0200

    Add upload script

---
+++ upload_all.sh
@@ -0,0 +1,10 @@
+ampy --port /dev/tty.usbmodem1A124411 put boot_sequence.py
+ampy --port /dev/tty.usbmodem1A124411 put motor.py
+ampy --port /dev/tty.usbmodem1A124411 put pixel_pump.py
+ampy --port /dev/tty.usbmodem1A124411 put settings_manager.py
+ampy --port /dev/tty.usbmodem1A124411 put ui_renderer.py
+ampy --port /dev/tty.usbmodem1A124411 put valve.py
+ampy --port /dev/tty.usbmodem1A124411 put button.py
+ampy --port /dev/tty.usbmodem1A124411 put main.py
+ampy --port /dev/tty.usbmodem1A124411 reset
+echo "ALL DONE. Board resetting."

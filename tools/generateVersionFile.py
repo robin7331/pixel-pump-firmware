ff84e9aa37f0a71df480e37e43fc7fa6f64ec342
Robin Reiter <robin7331@gmail.com>
2023-07-05 21:18:12 +0200

    Add latest version tag to generateVersionFile.py

--- tools/generateVersionFile.py
+++ tools/generateVersionFile.py
@@ -3,46 +3,51 @@ import subprocess
 import re
 from datetime import datetime, timezone
 
-def get_git_info(repo_path):
-    tag = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0'], cwd=repo_path).decode().strip()
-    tag = tag[1:]  # Remove the first character from the tag
-    commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=repo_path).decode().strip()
-    branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], cwd=repo_path).decode().strip()
+def get_git_info(repo_path):    
+    # Note: git describe doesn't work if no tag is available
+    try:
+        tag = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0'], cwd=repo_path).decode().strip()
+    except subprocess.CalledProcessError as er:
+        tag = ""
+    
+    try:
+      latest_version_tag = subprocess.check_output(['git', 'describe', '--tags', '--match="v[0-9]*"', '--abbrev=0'], cwd=repo_path).decode().strip()
+    except subprocess.CalledProcessError as er:
+      latest_version_tag = ""
 
-    # Extract major, minor, and fix versions from the tag
-    version_regex = r'(\d+)\.(\d+)\.(\d+)'
-    match = re.match(version_regex, tag)
-    if match:
-        major = match.group(1)
-        minor = match.group(2)
-        fix = match.group(3)
-    else:
-        major = minor = fix = 0
+    try:
+      commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=repo_path).decode().strip()
+    except subprocess.CalledProcessError as er:
+      commit_hash = ""
+
+    try:
+      branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], cwd=repo_path).decode().strip()
+    except subprocess.CalledProcessError as er:
+          branch = ""
 
     # Get timestamp of the latest commit
-    commit_timestamp = subprocess.check_output(['git', 'log', '-1', '--format=%ct'], cwd=repo_path).decode().strip()
-    commit_datetime = datetime.fromtimestamp(int(commit_timestamp), tz=timezone.utc)
-    formatted_timestamp = commit_datetime.astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC')
+    try:
+      commit_timestamp = subprocess.check_output(['git', 'log', '-1', '--format=%ct'], cwd=repo_path).decode().strip()
+      commit_datetime = datetime.fromtimestamp(int(commit_timestamp), tz=timezone.utc)
+      formatted_timestamp = commit_datetime.astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC')
+    except subprocess.CalledProcessError as er:
+      formatted_timestamp = ""
 
-    return tag, commit_hash, branch, major, minor, fix, formatted_timestamp
+    return tag, latest_version_tag, commit_hash, branch, formatted_timestamp
 
 def generate_version_file(output_path, repo_path):
-    tag, commit_hash, branch, major, minor, fix, formatted_timestamp = get_git_info(repo_path)
+    tag, latest_version_tag, commit_hash, branch, formatted_timestamp = get_git_info(repo_path)
     print ('Version information:')
     print (f'Tag: {tag}')
-    print (f'Commit hash: {commit_hash}')
+    print (f'Latest Version Tag: {latest_version_tag}')
     print (f'Branch: {branch}')
-    print (f'Major: {major}')
-    print (f'Minor: {minor}')
-    print (f'Fix: {fix}')
+    print (f'Commit hash: {commit_hash}')
     print (f'Timestamp: {formatted_timestamp}')
     version_file_contents = f'''# Auto-generated version information
 tag = "{tag}"
-commit_hash = "{commit_hash}"
+latest_version_tag = "{latest_version_tag}"
 branch = "{branch}"
-major = {major}
-minor = {minor}
-fix = {fix}
+commit_hash = "{commit_hash}"
 timestamp = "{formatted_timestamp}"
 '''
 

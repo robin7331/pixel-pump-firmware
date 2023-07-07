f20b49e008a37f336375bcfe23a2e71aed470467
Robin Reiter <robin7331@gmail.com>
2023-07-08 01:59:50 +0200

    Freeze the correct package

--- boards/PIXEL_PUMP/manifest.py
+++ boards/PIXEL_PUMP/manifest.py
@@ -1,4 +1,4 @@
 include("$(PORT_DIR)/boards/manifest.py")
 
-package("pixel_pump", base_path="../../src/pixel_pump")
+package("pixel_pump", base_path="../../src")
 module("main.py", base_path="../../src")
